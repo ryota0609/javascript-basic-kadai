afc46ffae43398b65f3b8a3b46e277c761697e12
ryota0609 <komugi3@ymail.ne.jp>
2023-11-19 03:17:33 +0900

    課題コミット

---
+++ bmi.js
@@ -0,0 +1,13 @@
+let sorceCategory;
+
+sorceCategory = '体重';
+
+let sorceNumber = 68;
+
+console.log(sorceCategory);
+console.log(sorceNumber);
+
+sorceCategory = '身長';
+sorceNumber = 1.7;
+
+console.log(68 / (1.7 * 1.7));
