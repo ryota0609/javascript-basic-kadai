a82f26166853477449fd25a789b5e11a874e9df6
ryota0609 <komugi3@ymail.ne.jp>
2023-11-19 13:56:27 +0900

    コンソール呼び出し用

--- calc.js
+++ calc.js
@@ -1,7 +1,7 @@
-let num =  Math.floor(Math.random() * 45);
+const num =  Math.floor(Math.random() * 45);
 console.log(num);
 
-if (num % 3 === 0)&&(num % 5 === 0) {
+if (num % 3 === 0 && num % 5 === 0) {
   console.log('“3と5の倍数です');
 }
 
