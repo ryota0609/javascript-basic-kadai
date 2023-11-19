9380268c41bd1f0a0536eab5b5d4ed055d3e5399
ryota0609 <komugi3@ymail.ne.jp>
2023-11-19 13:27:13 +0900

    倍数調整

--- calc.js
+++ calc.js
@@ -1,15 +1,15 @@
 let num =  Math.floor(Math.random() * 45);
 console.log(num);
 
-if (num % 3 === 0) {
+else if (num % 3 === 0) {
   console.log('3の倍数です');
 }
 
-if (num % 5 === 0) {
+else if (num % 5 === 0) {
   console.log('5の倍数です');
 }
 
-else if (num % 3 === 0|| num % 5 === 0) {
+if (num % 3 === 0)&&(num % 5 === 0) {
   console.log('“3と5の倍数です');
 }
 
