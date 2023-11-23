431abeaf64c4e31d448cdf6787ebd86c73e4ecf2
ryota0609 <komugi3@ymail.ne.jp>
2023-11-23 12:59:43 +0900

    関数計算コミット

---
+++ square.js
@@ -0,0 +1,11 @@
+const todayWeather = () => {
+  console.log('今日は晴れです');
+  console.log('今日は曇りです');
+  console.log('今日は雨です');
+}
+
+const calculateNumber = (number) => {
+  console.log(number * 2);
+}
+
+calculateNumber(50);
